33991c65a812e464a563ad862b44ae517752d3e7
programmingmentor <mentor@programmingmentor.com>
2024-04-16 16:53:35 +0000

    Scaffolding

--- t3gallery/src/app/page.tsx
+++ t3gallery/src/app/page.tsx
@@ -1,9 +1,24 @@
 import Link from "next/link";
 
+const mockUrls = [
+  'https://utfs.io/f/e749aeac-40a4-442b-8f00-eca3ee2a3e8f-22dgn.webp',
+  'https://utfs.io/f/27a5c5f4-bcdb-48c9-9cca-a21448492a6f-1xtz4.jpg',
+  'https://utfs.io/f/c1331ac4-9e3c-46eb-b171-e37578be32fe-dc4ah6.jpg'
+]
+
+const mockImages = mockUrls.map((url, idx) => ({
+  id: idx + 1,
+  url
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello gallery in progress
+    <main className="flex flex-wrap gap">
+      {mockImages.map(image => (
+        <div key={image.id} className="w-48 p-4">
+          <img src={image.url} alt="image" />
+        </div>
+      ))}
     </main>
   );
 }
